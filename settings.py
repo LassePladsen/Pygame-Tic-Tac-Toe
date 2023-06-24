aa99edcb4763bb13b128299f227e8a4f23e84500
Rogumann <lasse.pladsen@hotmail.com>
2023-06-25 00:49:48 +0200

    Added type hint for settings.py.get_resource_path()

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-def get_resource_path(relative_path):
+def get_resource_path(relative_path: str) -> str:
     """Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
